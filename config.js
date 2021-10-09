5899d5fea7af8efca0a9e05c1ad3f646d496103f
khoshow <khoshow.official@gmail.com>
2021-10-09 15:24:07 +0530

    add envv

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
+
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION
+export const API = publicRuntimeConfig.PRODUCTION=="true"
     ? publicRuntimeConfig.API_PRODUCTION
     : publicRuntimeConfig.API_DEVELOPMENT;
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
-export const DOMAIN = publicRuntimeConfig.PRODUCTION
+export const DOMAIN = publicRuntimeConfig.PRODUCTION=="true"
     ? publicRuntimeConfig.DOMAIN_PRODUCTION
     : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
 
---
+++ next.config.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+//For Production
+module.exports = {  
+    publicRuntimeConfig: {
+        APP_NAME: process.env.APP_NAME,
+        API_DEVELOPMENT: process.env.API_DEVELOPMENT,
+        API_PRODUCTION: process.env.API_PRODUCTION,
+        PRODUCTION:process.env.PRODUCTION,
+        DOMAIN_DEVELOPMENT: process.env.DOMAIN_DEVELOPMENT,
+        DOMAIN_PRODUCTION: process.env. DOMAIN_PRODUCTION,
+        FB_APP_ID: process.env. FB_APP_ID,
+        DISQUS_SHORTNAME: process.env.DISQUS_SHORTNAME,
+        GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
+        webpack5: true
+    }
+}
+
+//For Development
+
+// module.exports = {  
+//     publicRuntimeConfig: {
+//         APP_NAME: process.env.APP_NAME,
+//         API_DEVELOPMENT: process.env.API_DEVELOPMENT,
+//         API_PRODUCTION: process.env.API_PRODUCTION,
+//         PRODUCTION=process.env.PRODUCTION,
+//         DOMAIN_DEVELOPMENT: process.env.DOMAIN_DEVELOPMENT,
+//         DOMAIN_PRODUCTION: process.env. DOMAIN_PRODUCTION,
+//         FB_APP_ID: process.env. FB_APP_ID,
+//         DISQUS_SHORTNAME: process.env.DISQUS_SHORTNAME,
+//         GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
+//         webpack5: true
+//     }
+// }
+
